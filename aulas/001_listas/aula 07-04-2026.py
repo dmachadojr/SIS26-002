965792534d7d7e58c8e68cf5d26862067a67ec76
Dorival M. Machado Jr. <dorivaljunior@gmail.com>
2026-04-07 19:00:58 -0300

    aula 07-04-2026

---
+++ aulas/001_listas/aula 07-04-2026.py
@@ -0,0 +1,7 @@
+
+print("Exercício 1: Combinação de Uniformes (3 Listas e Condicionais)")
+'''
+Enunciado: Utilizando a lógica de combinações triplas dos fontes, crie três listas: camisas, calcas e calcados. O programa deve exibir as combinações, mas com as seguintes restrições:
+NÃO é permitida a combinação de "Camisa Verde" com "Calça Azul".
+A combinação só é válida se o calçado for "Tênis" OU se a camisa NÃO for "Social".
+'''
